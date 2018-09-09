8bc07fa21527fb1854ad024d5c04535bab776aa6
Ruslan Zaytsev <ruslan.zaytsev@gmail.com>
2018-09-09 05:05:34 +0300

    configuration through files

---
+++ scripts/helpers/getFraConfig.js
@@ -0,0 +1,19 @@
+const { join } = require('path')
+
+module.exports = () => {
+  const rootPackagePath = join(process.cwd(), 'package.json')
+  const rcPath = join(process.cwd(), '.frarc.json')
+  let rc
+  let packageConfig
+
+  try {
+    /* eslint-disable global-require, import/no-dynamic-require */
+    packageConfig = require(rootPackagePath).fra
+    rc = require(rcPath)
+  } catch (error) {
+    // no handling required
+  }
+  if (packageConfig) return packageConfig
+  if (rc) return rc
+  return null
+}
---
+++ spec/scripts/helpers/getFraConfig.spec.js
@@ -0,0 +1,51 @@
+const { join } = require('path')
+const getFraConfig = require('../../../scripts/helpers/getFraConfig')
+
+const rootPackagePath = join(process.cwd(), 'package.json')
+
+describe('getFraConfig', () => {
+  beforeEach(() => {
+    jest.resetAllMocks()
+    jest.resetModules()
+  })
+
+  it('returns null if neither ".frarc" and no "fra" field in package.json', () => {
+    jest.mock(rootPackagePath, () => ({
+      name: 'test-stub',
+      version: '2.3.8',
+    }), { virtual: true })
+    expect(getFraConfig()).toEqual(null)
+  })
+
+  it('returns config out of "fra" property in package.json', () => {
+    const mockData = {
+      mySweetConfig: 'isAweasome',
+      otherProperty: 'anotherValue',
+    }
+    jest.mock(rootPackagePath, () => ({
+      name: 'test-stub',
+      version: '2.3.8',
+      fra: mockData,
+    }), { virtual: true })
+    expect(getFraConfig()).toEqual(mockData)
+  })
+})
+
+describe('reads config out of ".frarc.json" file', () => {
+  it('reads config out of ".frarc.json" file', () => {
+    const mockData = {
+      exclude: '**/__tests__/**',
+      babel: {
+        presets: [
+          ['@babel/preset-stage-0', {
+            useBuiltIns: true,
+          }],
+        ],
+      },
+    }
+    jest.mock(rootPackagePath, () => ({ val: true }), { virtual: true })
+    jest.mock(join(process.cwd(), '.frarc.json'), () => mockData, { virtual: true })
+
+    expect(getFraConfig()).toEqual(mockData)
+  })
+})
